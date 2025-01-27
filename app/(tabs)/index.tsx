67f23e3b44177e5dea4039ece5b6d739aa78e600
Zeptosec <armandasmatkevicius@gmail.com>
2025-01-27 13:11:54 +0200

    Added eddystone scanning

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,37 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
+import { Image, StyleSheet, Platform, EventSubscription, Button } from 'react-native';
+import * as Beacons from 'expo-beacons';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useEffect, useState } from 'react';
 
 export default function HomeScreen() {
+  const [granted, setGranted] = useState<boolean>();
+
+  useEffect(() => {
+    let listener: any;
+    async function start() {
+      const perms = await Beacons.requestPermissionsAsync();
+
+      if (perms) {
+        await Beacons.startScanning([]);
+        listener = Beacons.addBeaconListener(event => {
+          console.log(event.data);
+        })
+      }
+    }
+    start();
+    return () => {
+      listener?.remove();
+    }
+  })
+
+  function updateRegions(uuids: string[]) {
+    console.log(`updated regions: ${uuids}`)
+    Beacons.updateMonitoredRegions(uuids);
+  }
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -15,6 +41,10 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
+      <Button title='UUIDS 0' onPress={() => updateRegions([])} />
+      <Button title='UUIDS 1' onPress={() => updateRegions(["16967031-4a76-9c35-886d-000050000035"])} />
+      <Button title='UUIDS 2' onPress={() => updateRegions(["16967031-4a76-9c35-886d-a0ba3b9eb045"])} />
+      <Button title='UUIDS 2' onPress={() => updateRegions(["16967031-4a76-9c35-886d-000050000035", "16967031-4a76-9c35-886d-a0ba3b9eb045"])} />
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
